7ddec912f4507273cae256da10ac6569dc0ad06f
hiroshi-nohara <noblessoblige443@gmail.com>
2016-09-01 22:59:13 +0900

    Create New Solution

---
+++ AVALON.Iekei.Persona/AVALON.Iekei.Persona/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AVALON.Iekei.Persona.Startup))]
+namespace AVALON.Iekei.Persona
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
